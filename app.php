5a427810ec2e640b05d474cbd2bc4b0be0ccd4ea
Thibaut Melen <tmelendesign@gmail.com>
2017-11-29 16:58:50 +0100

    sheel root

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "root");
 define("DB_DBNAME", "colony24"); // A DEFINIR
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
